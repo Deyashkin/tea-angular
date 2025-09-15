a02bc6f02e93ed98a5a0c3435cdc1bbffa8b7795
Dmitriy Deyashkin <d.deyashkin@gmail.com>
2025-09-15 23:12:47 +0300

    исправил слайдер и аккордион

--- src/app/components/pages/main/main.ts
+++ src/app/components/pages/main/main.ts
@@ -12,6 +12,37 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 })
 export class Main {
   showPopup = false;
+  currentSlide = 0;
+
+  slides = [
+    {
+      img: "slider-image-1.png",
+      alt: "Slide 1",
+      title: "Скидки на травянные чаи",
+      description: "Узнай все подробности, заполнив заявку",
+      buttonText: "Заполнить",
+      colClass: "col-7"
+    },
+    {
+      img: "slider-image-2_2.png",
+      alt: "Slide 2",
+      title: "Закажи три пачки чая<br>и получи подарок",
+      description: "",
+      buttonText: "Заполнить",
+      colClass: "col-7"
+    },
+    {
+      img: "slider-image-3.png",
+      alt: "Slide 3",
+      title: "Попробуй нашу новинку — ягодный чай",
+      description: "",
+      buttonText: "Заказать",
+      colClass: "col-5"
+    }
+  ];
+
+  private intervalId: any;
+
 
   constructor() {
     timer(10_000)
@@ -19,5 +50,51 @@ export class Main {
       .subscribe(() => this.showPopup = true);
   }
 
+  ngOnInit() {
+    this.startSlider();
+
+    // Инициализация WOW.js
+    if (typeof (window as any).WOW === 'function') {
+      new (window as any).WOW({
+        animateClass: 'animate__animated',
+      }).init();
+    }
+  }
+
+  startSlider() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Смена слайда каждые 5 секунд
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
   closePopup() { this.showPopup = false; }
+
+  openFaq = signal<number | null>(0);  // какой пункт открыт (по умолчанию 0) или null
+
+  toggleFaq(i: number) {
+    const cur = this.openFaq();
+    this.openFaq.set(cur === i ? null : i); // клик по открытому — закрываем
+  }
+
+  isOpen(i: number) {
+    return this.openFaq() === i;
+  }
 }
--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -27,36 +27,6 @@ $(document).ready(function () {
 
     });
 
-
-    $('.slick-slider').slick(
-            {
-                arrows: true,
-                infinite: true,
-                slidesToShow: 1,
-                prevArrow: '<button type="button" class="slick-prev"><svg width="20px" height="20px" viewBox="0 0 25 20" xmlns="http://www.w3.org/2000/svg"><path d="M5.32935 9.47742L13.7908 0.176819C13.9865 -0.0384587 14.2551 -0.164517 14.547 -0.178062C14.8389 -0.191607 15.1179 -0.0909537 15.3323 0.105292L16.0152 0.729469C16.4596 1.13622 16.4912 1.82934 16.0858 2.27487L8.98048 10.085L16.7758 17.212C16.9903 17.4082 17.116 17.6772 17.1294 17.9697C17.1427 18.2625 17.042 18.5421 16.8463 18.7575L16.2232 19.4421C16.0273 19.6574 15.7589 19.7835 15.467 19.797C15.1751 19.8106 14.8961 19.7099 14.6817 19.5137L5.40013 11.0282C5.18513 10.8313 5.05964 10.561 5.04692 10.268C5.03285 9.97387 5.13319 9.6932 5.32935 9.47742Z" fill="#595555"/></svg></button>',
-                nextArrow: '<button type="button" class="slick-next"><svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M16.5103 10.5226L8.04881 19.8232C7.85312 20.0385 7.58452 20.1645 7.2926 20.1781C7.00068 20.1916 6.72174 20.091 6.50726 19.8947L5.82438 19.2705C5.38 18.8638 5.34837 18.1707 5.75385 17.7251L12.8591 9.91505L5.0638 2.78805C4.84932 2.5918 4.72357 2.32276 4.71022 2.03028C4.69685 1.73747 4.79757 1.45792 4.99326 1.24248L5.6164 0.557867C5.81225 0.342583 6.08069 0.216532 6.37261 0.202987C6.66452 0.189442 6.94347 0.290095 7.15795 0.48634L16.4395 8.97182C16.6545 9.16869 16.78 9.43905 16.7927 9.73205C16.8068 10.0261 16.7064 10.3068 16.5103 10.5226Z" fill="#595555"/></svg></button>'
-            }
-        );
-
-        $(function () {
-            var icons = {
-                header: "ui-icon-circle-arrow-e",
-                activeHeader: "ui-icon-circle-arrow-s"
-            };
-            $("#accordion").accordion({
-                heightStyle: "content",
-                collapsible: true,
-                icons: icons
-            });
-            $("#toggle").button().on("click", function () {
-                if ($("#accordion").accordion("option", "icons")) {
-                    $("#accordion").accordion("option", "icons", null);
-                } else {
-                    $("#accordion").accordion("option", "icons", icons);
-                }
-            });
-        });
-
         let indexInput = $('#index');
 
         indexInput.on("input", function () {
@@ -78,56 +48,6 @@ $(document).ready(function () {
             }
 
             this.value = newValue;
-        });
-
-        $('#form-button').click(function (event) {
-            event.preventDefault();
-            let nameInput = $('#name');
-            let surnameInput = $('#surname');
-            let phoneInput = $('#phone');
-            let countryInput = $('#country');
-            let addressInput = $('#address');
-
-            if (!nameInput.val()) {
-                alert('Заполните поле "Имя"');
-                return;
-            }
-            if (!surnameInput.val()) {
-                alert('Заполните поле "Фамилия"');
-                return;
-            }
-            if (!phoneInput.val()) {
-                alert('Заполните поле "Телефон"');
-                return;
-            }
-            if (!countryInput.val()) {
-                alert('Заполните поле "Страна"');
-                return;
-            }
-            if (!indexInput.val()) {
-                alert('Заполните поле "Индекс"');
-                return;
-            }
-            if (indexInput.val().length !== 6) {
-                alert('Поле "Индекс" обязательно должно быть длинной в 6 цифр');
-                return;
-            }
-            if (!addressInput.val()) {
-                alert('Заполните поле "Адресс"');
-                return;
-            }
-
-            $(indexInput).on('keypress', function (e) {
-                const numberValue = Number(e.key);
-                if (!isNaN(numberValue)) {
-                    e.preventDefault();
-                }
-            });
-
-            $('.order-form').hide();
-            $('#thank-you-message').show();
-
-            alert('Спасибо за заказ!')
         });
     }
 )
